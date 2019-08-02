67b565e0e901874bf365cc688edaa20e5cb09d00
zenunfelipe <siul.zenun.05@gmail.com>
2019-08-02 15:01:19 -0400

    prueba stock bvn7

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1368,7 +1368,7 @@ angular.module('andes.controllers', [])
                   Descripcion: $scope.info.ConteoInventario[i].Nombre,
                   IDArticulo: $scope.info.ConteoInventario[i].IDArticulo,
                   Bulto: $scope.info.ConteoInventario[i].StockUnidadxBulto,
-                  Cantidad: $scope.info.ConteoInventario[i].StockUnidadxBulto
+                  Cantidad: $scope.info.InfoBPMArticulo[0].StockUnidadxBulto
                 });
 
               }
